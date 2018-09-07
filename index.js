23688d8f8a5207560aeee1741cd4705dc0782b82
jgk83 <jgk83@cornell.edu>
2018-09-07 03:33:26 -0400

    Updated shamebot

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const config = require("./config.json");
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const roman = require('roman-numerals');
 
-let shame = false;
+let match_nickname = /Diogenes ([XVLICMD]+)/;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("Watching... Waiting...");
+    client.user.setActivity("Getting Over It");
 });
 
 client.on('message', msg => {
@@ -15,8 +16,22 @@ client.on('message', msg => {
   }
 });
 
-client.on('memberUpdate', usr => {
-    console.log("recieved.");
-    console.log(usr);
+client.on('guildMemberUpdate', (ousr, nusr) => {
+    if(ousr.user.id == config.shameUserId && ousr.nickname != nusr.nickname){
+        let new_nickname = nusr.nickname;
+        let matches = new_nickname.match(match_nickname);
+        try{
+            let newNumber = 50;
+            if(matches.length > 1 && (newNumber = roman.toArabic(matches[1]))){
+                ousr.guild.members.get(client.user.id).setNickname("ShameBot " + roman.toRoman(newNumber + 1));
+                ousr.guild.channels.find(ch => ch.name === 'general').send(config.shameMessage);
+            }
+        }catch(err){
+            console.log(err);
+        }
+
+
+    }
+
 });
 client.login(config.token);
