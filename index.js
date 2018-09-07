64b7c5dbfc427fe59d1ba9770f80fd73eb1dc89a
jgk83 <jgk83@cornell.edu>
2018-09-07 01:23:56 -0400

    Updated shamebot slightly with a ping pong

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+  if (msg.content === 'does it live?') {
+    msg.reply('It lives.');
+  }
+});
 client.login(config.token);
